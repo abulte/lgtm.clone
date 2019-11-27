6c0838f5a9e1098fb50dd28d4586f5404160a5f5
Alexandre Bulté <alexandre@bulte.net>
2019-11-27 16:53:42 +0100

    first version

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>lgtm.clone</h1>
+    <div v-for="image in images" v-bind:key="image.id">
+      <img :src="image.images.fixed_height.url">
+      <pre>
+        ![]({{ image.images.fixed_height.url }})
+      </pre>
+    </div>
   </div>
 </template>
 
@@ -12,6 +17,39 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      images: [],
+      limit: 8,
+      // what giphy has to offer
+      maxResults: 50
+    }
+  },
+  computed: {
+    offset () {
+      return this.page * this.limit
+    },
+    page () {
+      return this.getRandomInt(this.maxResults / this.limit)
+    }
+  },
+  methods: {
+    getRandomInt (max) {
+      return Math.floor(Math.random() * Math.floor(max))
+    },
+    shuffle (array) {
+      return array.sort(() => Math.random() - 0.5)
+    }
+  },
+  mounted () {
+    const api_key = '0aqJyd4OFUcq2tZ3tgAvDSC02DnEBgds'
+    const q =  'lgtm'
+    this.$http.get(`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${q}&offset=${this.offset}&limit=${this.limit}`).then(res => {
+      return res.json()
+    }).then(res => {
+      this.images = this.shuffle(res.data)
+    })
   }
 }
 </script>
